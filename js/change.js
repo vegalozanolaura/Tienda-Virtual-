ea51c8251beb54b64af68463f2f975aefb142df0
vegalozanolaura <106939179+vegalozanolaura@users.noreply.github.com>
2022-11-22 18:40:16 -0500

    Update change.js

--- js/change.js
+++ js/change.js
@@ -6,3 +6,17 @@ function changecircle(color){
     const circle = document.querySelector(".circle");
     circle.style.background = color;
 }
+
+let products = [];
+let total = 0;
+
+function add(product, price) {
+    console.log(product, price);
+    products.push(product);
+    total = total + price;
+    document.getElementById("checkout").innerHTML = `Pagar $${total}`
+}
+
+function pay() {
+    window.alert(products.join(", \n"));
+}
